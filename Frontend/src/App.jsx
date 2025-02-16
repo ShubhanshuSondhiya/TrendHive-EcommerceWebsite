f909393c1b8388f3af50e64b324c0d8163e3b919
Shubhanshu Sondhiya <shubhanshusondhiya@gmail.com>
2025-02-16 17:33:22 +0530

    header

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,12 +1,17 @@
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import UserLayout from "./components/Layout/UserLayout";
 
 const App = () => {
   return (
     <div>
-      
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<UserLayout />}></Route>
+          <Route></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
-  )
-}
+  );
+};
 
-export default App
-
+export default App;
