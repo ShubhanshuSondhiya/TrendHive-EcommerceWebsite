502ef37c2b291dcbf06b2f5f106dc4df1d403993
Shubhanshu Sondhiya <shubhanshusondhiya@gmail.com>
2025-02-23 23:11:38 +0530

    Product api done with filter functionality

--- Backend/routes/productRoutes.js
+++ Backend/routes/productRoutes.js
@@ -118,4 +118,96 @@ router.put("/:id", protect, admin, async (req, res) => {
   }
 });
 
+router.delete("/:id", protect, admin, async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
+    if (product) {
+      await product.deleteOne();
+      res.json({ message: "Product removed" });
+    } else {
+      res.status(404).json({ message: "Product not found" });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Server Error");
+  }
+});
+
+router.get("/", async (req, res) => {
+  try {
+    const {
+      collection,
+      size,
+      color,
+      gender,
+      minPrice,
+      maxPrice,
+      sortBy,
+      search,
+      category,
+      material,
+      brand,
+      limit,
+    } = req.query;
+
+    let query = {};
+    if (collection && collection.toLocaleLowerCase() !== "all") {
+      query.collections = collection;
+    }
+    if (category && category.toLocaleLowerCase() !== "all") {
+      query.category = category;
+    }
+    if (material) {
+      query.material = { $in: material.split(",") };
+    }
+    if (brand) {
+      query.brand = { $in: brand.split(",") };
+    }
+    if (size) {
+      query.sizes = { $in: size.split(",") };
+    }
+    if (color) {
+      query.color = { $in: [color] };
+    }
+    if (gender) {
+      query.gender = gender;
+    }
+    if (minPrice || maxPrice) {
+      query.price = {};
+      if (minPrice) query.price.gte = Number(minPrice);
+      if (maxPrice) query.price.lte = Number(maxPrice);
+    }
+    if (search) {
+      query.$or = [
+        { name: { $regex: search, $options: "i" } },
+        { description: { $regex: search, $options: "i" } },
+      ];
+    }
+    let sort = {};
+    if (sortBy) {
+      switch (sortBy) {
+        case "priceAsc":
+          sort = { price: 1 };
+          break;
+        case "priceDesc":
+          sort = { price: -1 };
+          break;
+        case "popularity":
+          sort = { rating: -1 };
+          break;
+        default:
+          break;
+      }
+    }
+
+    let products = await Product.find(query)
+      .sort(sort)
+      .limit(Number(limit) || 0);
+    res.json(products);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Server Error");
+  }
+});
+
 export default router;
---
+++ Backend/seeder.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import Product from "./models/Product.js";
+import User from "./models/User.js";
+import products from "./data/products.js";
+
+dotenv.config();
+mongoose.connect(process.env.MONGO_URI);
+
+const seedData = async () => {
+  try {
+    await Product.deleteMany();
+    await User.deleteMany();
+
+    const createdUser = await User.create({
+      name: "Admin User",
+      email: "admin@example.com",
+      password: "123456",
+      role: "admin",
+    });
+
+    const userId = createdUser._id;
+    const sampleProducts = products.map((product) => {
+      return { ...product, user: userId };
+    });
+    await Product.insertMany(sampleProducts);
+    console.log("Project data seeded successfully");
+    process.exit();
+  } catch (error) {
+    console.log("Error Seeding the data");
+    process.exit(1);
+  }
+};
+
+seedData();
