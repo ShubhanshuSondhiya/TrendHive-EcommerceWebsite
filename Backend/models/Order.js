a3d385a6b6795c48edc6580511adc957616ad930
Shubhanshu Sondhiya <shubhanshusondhiya@gmail.com>
2025-03-01 00:27:55 +0530

    Admin Order Management Bug Fix.

--- Backend/models/Order.js
+++ Backend/models/Order.js
@@ -74,6 +74,7 @@ const orderSchema = new mongoose.Schema(
     status: {
       type: String,
       enum: ["Processing", "Shipped", "Delivered", "Cancelled"],
+      default: "Processing",
     },
   },
   {
