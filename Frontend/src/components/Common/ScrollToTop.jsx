f8cb856467504429ebc142ade4a947e2bd1a464b
Shubhanshu Sondhiya <shubhanshusondhiya@gmail.com>
2025-03-10 08:25:22 +0530

    Button changes and Link path placed

--- Frontend/src/components/Common/ScrollToTop.jsx
+++ Frontend/src/components/Common/ScrollToTop.jsx
@@ -1,11 +1,15 @@
-import { useEffect } from "react";
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 
 const ScrollToTop = () => {
+  // Extracts pathname property(key) from an object
+  const { pathname } = useLocation();
+
+  // Automatically scrolls to top whenever pathname changes
   useEffect(() => {
     window.scrollTo({ top: 0, behavior: "smooth" });
-  }, []);
 
-  return null;
-};
+  }, [pathname]);
+}
 
 export default ScrollToTop;
--- Frontend/src/components/Layout/Hero.jsx
+++ Frontend/src/components/Layout/Hero.jsx
@@ -19,11 +19,16 @@ const Hero = () => {
           <p className="md:text-lg mb-6">
             Explore our vacation-ready outfits with worldwide shipping.
           </p>
-          <Link
-            to="#"
-            className="bg-white text-gray-950 px-6 py-2 rounded-sm text-lg hover:bg-red-700 hover:text-white"
-          >
-            Shop Now
+          <Link to="collections/all?category=Bottom%20Wear">
+            <a
+              href="#_"
+              className="relative inline-flex items-center justify-start px-6 py-3 overflow-hidden font-medium transition-all bg-white rounded hover:bg-white group"
+            >
+              <span className="w-48 h-48 rounded rotate-[-40deg] bg-red-700 absolute bottom-0 left-0 -translate-x-full ease-out duration-500 transition-all translate-y-full mb-9 ml-9 group-hover:ml-0 group-hover:mb-32 group-hover:translate-x-0"></span>
+              <span className="relative w-full text-left text-black transition-colors duration-300 ease-in-out group-hover:text-white">
+                Shop Now
+              </span>
+            </a>
           </Link>
         </div>
       </div>
